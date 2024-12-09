c71a018c97397994c6af6bab6e16a433f694b350
James Brown <b.james322@gmail.com>
2024-12-08 21:21:15 -0500

    update routes

--- routes/web.php
+++ routes/web.php
@@ -7,11 +7,6 @@ use Livewire\Volt\Volt;
 Route::get('/', HomeController::class)->middleware(['guest'])->name('home');
 
 Volt::route('goals', 'pages.goals.index')->middleware(['auth'])->name('goals.index');
-Volt::route('goals/{goal}', 'pages.goals.show')->middleware(['auth'])->name('goals.show');
-
-Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
 
 Route::view('profile', 'profile')
     ->middleware(['auth'])
